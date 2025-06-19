bf068fcaf617a69302376b05c93cc1b083a807c4
krish1124007 <148999167+krish1124007@users.noreply.github.com>
2025-06-19 11:56:19 +0530

    add cors

--- src/app.js
+++ src/app.js
@@ -2,13 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import { bank_router } from "./routes/bank.routes.js";
 import { admin_router } from "./routes/admin.routes.js";
-
+import cors from "cors"
 dotenv.config({});
+
 
 
 const app = express();
 //middlewares
 app.use(express.json());
+app.use(cors({
+    origin:"*"
+}))
 
 //routers
 app.use('/api/v1/bank',bank_router);
--- src/controllers/admin/admin.controller.js
+++ src/controllers/admin/admin.controller.js
@@ -64,3 +64,4 @@ export {
     loginAdmin,
     createAdmin
 }
+
