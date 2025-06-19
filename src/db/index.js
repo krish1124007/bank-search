ed994521109e31138ba0b2bf8731a09cb56dac68
krish1124007 <148999167+krish1124007@users.noreply.github.com>
2025-06-19 11:06:19 +0530

    add live url of mongodb

--- src/db/index.js
+++ src/db/index.js
@@ -5,12 +5,13 @@ import mongoose from "mongoose";
 async function connectDB()
 {
     try {
-        const connect =  await mongoose.connect('mongodb://localhost:27017/');
+        const connect =  await mongoose.connect(process.env.MONGO_DB_URL);
         console.log("Database connect successfully");
         return connect;
     } catch (error) {
         console.log("Something error is generate while connecting db");
     }
 }
+
 
 export { connectDB }
