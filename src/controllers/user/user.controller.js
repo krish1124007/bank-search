e949ec45b5c7ebebd78f30e7c4c52e6adb659533
krish1124007 <148999167+krish1124007@users.noreply.github.com>
2025-06-25 15:00:20 +0530

    Update user.controller.js
    add funtcion for the save user object

--- src/controllers/user/user.controller.js
+++ src/controllers/user/user.controller.js
@@ -1,1 +1,29 @@
-//this code push after some time
+import {}  from "../../utils/asyncHandler.js"
+
+
+
+
+const saveUserInforamtion = asyncHandler(async()=>{
+  const {username , contact_number } = req.body;
+  if(!username && !contact_number)
+  {
+    return res.status(400)
+    .json(
+      new ApiResponse(400,"please enter the username and password",{success:false , data:"usernameAndPasswordNullError"})
+    )
+  }
+
+  const userInfoSave = await User.create({username , contact_number});
+
+  if(!userInfoSave)
+  {
+    return res.status(500)
+    .json(
+      new ApiResponse(500 ,"something problem with the save user data" ,{success:false , data:"userObjectNotSaveError"})
+    )
+
+    return res.status(200)
+    .json(
+      new ApiResponse(200,"user object save successfully",{success:true , data:userInfoSave})
+  
+})
